39857271bf6d5289d50406b5cde59a5a75173a5a
Steven Grady <stevengrady@fb.com>
2022-06-13 15:32:59 -0700

    Querying the Parse database + theme changes

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -4,18 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "abc123";
     public Button btnlogOut;
     public ParseUser currentUser;
     public EditText etDescription;
     public Button btnTakePicture;
     public Button btnSubmit;
+    public ImageView ivPostImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnlogOut = findViewById(R.id.btnlogOut);
         etDescription = findViewById(R.id.etDescription);
+        ivPostImage = findViewById(R.id.ivPostImage);
         btnTakePicture = findViewById(R.id.btnTakePicture);
         btnSubmit = findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +50,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        queryPosts();
     }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                Log.e(TAG, "Issue with getting posts", e);
+                return;
+                }
+                for(Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+
+        });
+    }
+
     private void goLoginActivity() {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("7r1pbgBBBvtSXWPHMwntpyZVc6D6zTZz2Ah6z2MR")
